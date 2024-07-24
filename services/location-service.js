91eedaaaf1146ac08517d219de143a168d42dff7
Radu Baroncea <baroncea.radu@gmail.com>
2024-07-24 17:25:16 +0300

    bypass for ip api

--- services/location-service.js
+++ services/location-service.js
@@ -1,5 +1,7 @@
 const fetchLocation = async () => {
-  const response = await fetch(`https://ip-api.com/json/`)
+  const response = await fetch(
+    `https://corsproxy.io/?http%3A%2F%2Fip-api.com%2Fjson`
+  )
     .then((res) => res.json())
     .catch(() => {
       throw new Error("There was an error!");
